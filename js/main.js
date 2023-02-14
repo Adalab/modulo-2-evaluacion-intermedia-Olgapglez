0bf18d6947acc1df1eee097ab7f25b36fbb8e752
Olgapglez <parras.olga@gmail.com>
2023-02-14 09:15:15 +0100

    add body html

---
+++ js/main.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/* Pasos a ejecutar
+
+	1.Crear HTML y enlazar el JS
+	2.Escribir mi HTML
+	3.Cojo los elementos del HTML desde JS (añadir classes en HTML con "js-xxxx") 
+	4.Evento sobre boton click
+	5.Crear funcion y programar todo
+*/
+
+//constantes y variables
+//ejemplo: const camelCase = document.querySelcetor(".js-clasedehtml");
+
+
+//funciones
+//ejemplo: function handleClick(event) { }
+//ejemplo arrow: function handleClick = (event)=> { }
+
+
+//eventos
+//ejemplo: btn.addEventListener("click" , handleClick);
+
+
+//llamar a las funciones
