f72c20e8d265529095ac2036b9dddd19c33144b4
Olgapglez <parras.olga@gmail.com>
2023-02-14 11:01:34 +0100

    add conditionals

--- js/main.js
+++ js/main.js
@@ -10,16 +10,41 @@
 */
 
 //constantes y variables
-//ejemplo: const camelCase = document.querySelcetor(".js-clasedehtml");
+
+const inputNumber = document.querySelector(".js-inputNumber");
+const inputNumberValue = inputNumber.value;
+console.log (inputNumberValue);
+
+const button = document.querySelector(".js-button");
+const clue  = document.querySelector(".js-clue");
+const attempts = document.querySelector(".js-attempts");
+const randNumber = getRandomNumber (100);
+console.log (randNumber);
 
 
 //funciones
-//ejemplo: function handleClick(event) { }
-//ejemplo arrow: function handleClick = (event)=> { }
+
+//Function random number
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+    }
+
+function handleClick() {
+    if (inputNumberValue === randNumber) {
+        clue.innerHTML = "Has ganado campeona!!!";
+    } else if (inputNumberValue < randNumber) {
+        clue.innerHTML = "Demasiado bajo.";
+    } else if (inputNumberValue > randNumber) {
+        clue.innerHTML = "Demasiado alto.";
+    } else if (inputNumberValue <= 0) {
+        clue.innerHTML = "El número debe estar entre 1 y 100";
+    } else if (inputNumberValue > 100) {
+        clue.innerHTML = "El número debe estar entre 1 y 100";
+    }
+}
 
 
 //eventos
-//ejemplo: btn.addEventListener("click" , handleClick);
+button.addEventListener ("click", handleClick);
 
 
-//llamar a las funciones
